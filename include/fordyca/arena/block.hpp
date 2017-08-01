f548df7de529f4127f9ae3f91435fe0ccbfc2339
John Harwell <john.r.harwell@gmail.com>
2017-08-01 15:43:07 -0500

    Scaffold some of the classes that will be needed for representing each robot's view of the world/arena.

---
+++ include/fordyca/arena/block.hpp
@@ -0,0 +1,52 @@
+/**
+ * @file block.hpp
+ *
+ * @copyright 2017 John Harwell, All rights reserved.
+ *
+ * This file is part of FORDYCA.
+ *
+ * FORDYCA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * FORDYCA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * FORDYCA.  If not, see <http://www.gnu.org/licenses/
+ */
+
+#ifndef INCLUDE_FORDYCA_ARENA_BLOCK_HPP_
+#define INCLUDE_FORDYCA_ARENA_BLOCK_HPP_
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "rcppsw/common/common.hpp"
+
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+NS_START(fordyca);
+
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+class block {
+ public:
+  /* constructors */
+  block();
+
+  /* member functions */
+
+  
+ private:
+  /* member functions */
+
+  /* data members */
+};
+
+NS_END(forydca);
+#endif /* INCLUDE_FORDYCA_ARENA_BLOCK_HPP_ */
