5570e5d519878ba6b4b466d25622e7601ee6aaa9
John Harwell <john.r.harwell@gmail.com>
2017-08-11 21:26:05 -0500

    Address #3.

--- include/fordyca/controller/foraging_controller.hpp
+++ include/fordyca/controller/foraging_controller.hpp
@@ -60,6 +60,7 @@ class foraging_controller : public argos::CCI_Controller,
   bool is_returning(void) { return m_fsm->is_returning(); }
   bool is_avoiding_collision(void) { return m_fsm->is_avoiding_collision(); }
   bool in_nest(void) {return m_sensors->in_nest(); }
+  bool block_detected(void) { return m_sensors->block_detected(); }
   void publish_event(enum event_type event);
 
   /*
