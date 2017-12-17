79adc19361b16e69ce6a407d567b90d014ba6eed
John Harwell <john.r.harwell@gmail.com>
2017-12-17 17:44:59 -0600

    Address #214: Add always/never partition options for partitionable tasks

--- src/params/depth1/task_parser.cpp
+++ src/params/depth1/task_parser.cpp
@@ -39,20 +39,29 @@ void task_parser::parse(argos::TConfigurationNode& node) {
 
   argos::GetNodeAttribute(task_node, "estimation_alpha",
                           m_params->tasks.estimation_alpha);
-  argos::GetNodeAttribute(task_node, "abort_reactivity", m_params->tasks.abort_reactivity);
-  argos::GetNodeAttribute(task_node, "abort_offset", m_params->tasks.abort_offset);
-  argos::GetNodeAttribute(task_node, "partition_reactivity", m_params->tasks.partition_reactivity);
-  argos::GetNodeAttribute(task_node, "partition_offset", m_params->tasks.partition_offset);
+  argos::GetNodeAttribute(task_node, "abort_reactivity",
+                          m_params->tasks.abort_reactivity);
+  argos::GetNodeAttribute(task_node, "abort_offset",
+                          m_params->tasks.abort_offset);
+  argos::GetNodeAttribute(task_node, "partition_reactivity",
+                          m_params->tasks.partition_reactivity);
+  argos::GetNodeAttribute(task_node, "partition_offset",
+                          m_params->tasks.partition_offset);
   argos::GetNodeAttribute(task_node, "subtask_selection_method",
                           m_params->tasks.subtask_selection_method);
   argos::GetNodeAttribute(task_node, "partition_method",
                           m_params->tasks.partition_method);
+  argos::GetNodeAttribute(task_node, "always_partition",
+                          m_params->tasks.always_partition);
+  argos::GetNodeAttribute(task_node, "never_partition",
+                          m_params->tasks.never_partition);
+
   argos::GetNodeAttribute(task_node, "init_random_estimates",
                           m_params->init_random_estimates);
 } /* parse() */
 
 void task_parser::show(std::ostream& stream) {
-  stream << "====================\nTASK params\n====================\n";
+  stream << "====================\nTask params\n====================\n";
   stream << "estimation_alpha=" << m_params->tasks.estimation_alpha << std::endl;
   stream << "abort_reactivity=" << m_params->tasks.abort_reactivity << std::endl;
   stream << "abort_offset=" << m_params->tasks.abort_offset << std::endl;
@@ -60,6 +69,8 @@ void task_parser::show(std::ostream& stream) {
   stream << "partition_offset=" << m_params->tasks.partition_offset << std::endl;
   stream << "subtask_selection_method=" << m_params->tasks.subtask_selection_method << std::endl;
   stream << "partition_method=" << m_params->tasks.partition_method << std::endl;
+  stream << "always_partition=" << m_params->tasks.always_partition << std::endl;
+  stream << "never_partition=" << m_params->tasks.never_partition << std::endl;
   stream << "init_random_estimates=" << m_params->init_random_estimates << std::endl;
 } /* show() */
 
